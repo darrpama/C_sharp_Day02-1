b31c7f37f95a8fd74625e0412ba986538e41870c
darrpama <darrpama@darrpamas-MacBook-Air.local>
2024-07-04 01:00:28 +0300

    add task type

---
+++ src/d02_ex01/Tasks/TaskType.cs
@@ -0,0 +1,60 @@
+using System.Runtime.CompilerServices;
+
+public class Task
+{
+    public string Title { get; private set; }
+    public string? Summary { get; private set; }
+    public DateTime? DueDate { get; private set; }
+    public TaskType Type { get; private set; }
+    public TaskPriority Priority { get; private set; }
+    public TaskState State { get; private set; }
+
+    public Task(string title, TaskType type, DateTime? dueDate = null, string? summary = null,
+                 TaskPriority priority = TaskPriority.Normal)
+    {
+        Title = title;
+        Summary = summary;
+        DueDate = dueDate;
+        Type = type;
+        Priority = priority;
+        State = TaskState.New;
+    }
+
+    public override string ToString()
+    {
+        string answer = "";
+        if (DueDate == null) {
+            answer = $"- {Title}\n" +
+                     $"[{Type}] [{State}]\n" +
+                     $"Priority: {Priority}\n" +
+                     $"{Summary}";
+
+        } else {
+            answer = $"- {Title}\n" +
+                    $"[{Type}] [{State}]\n" +
+                    $"Priority: {Priority}, Due till {DueDate}\n" +
+                    $"{Summary}";
+        }
+        return answer;
+    }
+
+    public bool MarkAsCompleted()
+    {
+        bool answer = false;
+        if (State == TaskState.New) {
+            State = TaskState.Completed;
+            answer = true;
+        }
+        return answer;
+    }
+
+    public bool MarkAsIrrelevant()
+    {
+        bool answer = false;
+        if (State == TaskState.New) {
+            State = TaskState.Irrelevant;
+            answer = true;
+        }
+        return answer;
+    }
+}
