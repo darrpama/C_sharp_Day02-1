7a05e6aba1fb50fc64cb34ac77eb33c6bf72bdad
Darr Pamala <darrpama@si-c2.kzn.21-school.ru>
2024-07-15 21:53:54 +0300

    fix exceptions

--- src/d02_ex00/Models/ExchangeSum.cs
+++ src/d02_ex00/Models/ExchangeSum.cs
@@ -1,4 +1,5 @@
-public struct ExchangeSum {
+public struct ExchangeSum
+{
     public decimal Amount { get; set; }
     public string Currency { get; set; }
 
@@ -7,14 +8,20 @@ public struct ExchangeSum {
         return ($"{Amount:N2} {Currency}");
     }
     
-    static public bool TryParse(string input, out ExchangeSum exchange) {
+    static public bool TryParse(string input, out ExchangeSum exchange)
+    {
         bool result = false;
         exchange = default;
         var tokens = input.Split(' ');
-        if (tokens.Length == 2 && decimal.TryParse(tokens[0], out var amount)) {
+        if (tokens.Length == 2 && decimal.TryParse(tokens[0], out var amount) && tokens[1].Length == 3)
+        {
             var currency = tokens[1];
             exchange = new ExchangeSum { Amount = amount, Currency = currency };
             result = true;
+        }
+        else
+        {
+            throw new ArgumentException("Input error. Check the input data and repeat the request.");
         }
         return result;
     }
--- src/d02_ex00/Program.cs
+++ src/d02_ex00/Program.cs
@@ -1,16 +1,27 @@
-var sum = args[0];
-var directory = args[1];
 
-if (ExchangeSum.TryParse(sum, out var inputSum) && Directory.Exists(directory))
+try
 {
+    if (args.Length != 2)
+    {
+        throw new ArgumentException("Input error. Check the input data and repeat the request.");
+    }
+
+    var sum = args[0];
+    var directory = args[1];
+
+    if (!Directory.Exists(directory))
+    {
+        throw new ArgumentException("Input error. Check the input data and repeat the request.");
+    }
+    ExchangeSum.TryParse(sum, out var inputSum);
+
     Console.WriteLine($"Amount in the original currency: {inputSum}");
     Exchanger exchanger = new Exchanger(args[1]);
     foreach (var convertedSum in exchanger.Convert(inputSum))
     {
         Console.WriteLine($"Amount in {convertedSum.Currency}: {convertedSum:N2}");
     }
-}
-else
-{
-    Console.WriteLine("Input error. Check the input data and repeat the request.");
+
+} catch (Exception e) {
+    Console.WriteLine(e.Message);
 }
